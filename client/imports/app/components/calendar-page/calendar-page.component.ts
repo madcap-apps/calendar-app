80fc5e32abf42992bec953dca5fb2360bff42cb6
Jason Ware <jason.christopher.ware@gmail.com>
2019-06-11 16:23:16 -0700

    initial checkin of sample implementation and some empty UI components for guidance

---
+++ client/imports/app/components/calendar-page/calendar-page.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'mc-calendar-page',
+  templateUrl: './calendar-page.component.html',
+  styleUrls: ['./calendar-page.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class CalendarPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ client/imports/app/components/event-page/event-page.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'mc-event-page',
+  templateUrl: './event-page.component.html',
+  styleUrls: ['./event-page.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class EventPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
