ef8dfcf991702aa665dc59927705994b49fc88fe
Jason Ware <jason.christopher.ware@gmail.com>
2019-09-19 12:43:22 -0700

    Including config file used for building and publishing

---
+++ mobile-config.js
@@ -0,0 +1,12 @@
+// This section sets up some basic app metadata, the entire section is optional.
+App.info({
+  id: 'com.madcap.madgathering',
+  name: 'MadGathering',
+  version: '0.0.2',
+  description: 'event based app for MadCap Software employees',
+  author: 'MadCap Software',
+  email: 'madcap.software.community.service@gmail.com',
+  website: 'https://madcap-calendar-app.herokuapp.com/'
+});
+
+App.setPreference('android-targetSdkVersion', '28');
